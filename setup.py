b57a59594daef804608c7d449ad16a370219edb6
Eshant Singh <eshant@live.com>
2026-06-27 01:02:31 +0530

    this is my first commit

---
+++ setup.py
@@ -0,0 +1,25 @@
+# setup tools is basically a package where have a information of a project
+from setuptools import find_packages, setup
+from typing import List
+
+HYPHEN_E_DOT = '-e.'
+
+def get_requirments(file_path: str) -> List[str]:
+    requirments = []
+    with open(file_path) as file_obj:
+        requirments = file_obj.readlines()
+        requirments = [req.replace("\n", "") for req in requirments]
+
+    if HYPHEN_E_DOT in requirments:
+        requirments.remove(HYPHEN_E_DOT)
+    return requirments
+
+
+setup(
+    name= "Fault detection",
+    version= '0.0.1',
+    author= 'Pwskills',
+    author_email= 'es.@pw.live',
+    install_requires= get_requirments('requirments.txt'),
+    packages= find_packages() 
+)
